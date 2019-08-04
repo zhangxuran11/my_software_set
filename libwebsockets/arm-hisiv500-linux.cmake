f553070c43a839bd940fcdd4737a777927ae7fd8
zxr <zxr13027502030@163.com>
2019-08-05 03:05:45 +0800

    xxx

---
+++ libwebsockets/arm-hisiv500-linux.cmake
@@ -0,0 +1,39 @@
+SET(CROSS_COMPILE 1)
+set (CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_SOURCE_DIR}/install" CACHE PATH "")
+IF(CROSS_COMPILE) 
+SET(CMAKE_SYSTEM_NAME Linux)
+
+SET(TOOLCHAIN_DIR "/opt/hisi-linux/x86-arm/arm-hisiv500-linux")
+
+#SET(OPENSSL_ROOT_DIR  "${TOOLCHAIN_DIR}/target/lib" )
+#SET(OPENSSL_CRYPTO_LIBRARY "${TOOLCHAIN_DIR}/target/lib")
+#SET(LWS_OPENSSL_INCLUDE_DIRS  "${TOOLCHAIN_DIR}/target/include/" )
+#SET(LWS_OPENSSL_LIBRARIES  "${TOOLCHAIN_DIR}/target/lib/libssl.so;${TOOLCHAIN_DIR}/target/lib/libcrypto.so" )
+SET(LWS_OPENSSL_INCLUDE_DIRS  "/opt/hisi-linux/x86-arm/arm-hisiv500-linux/target/include/" CACHE PATH "Path to the OpenSSL include directory")
+SET(LWS_OPENSSL_LIBRARIES  "/opt/hisi-linux/x86-arm/arm-hisiv500-linux/target/lib/libssl.so;/opt/hisi-linux/x86-arm/arm-hisiv500-linux/target/lib/libcrypto.so" CACHE PATH "Path to the OpenSSL library" )
+#SET(OPENSSL_VERSION "1.0.2sss")
+#SET(ZLIB_LIBRARY "${TOOLCHAIN_DIR}/target/lib")
+#SET(ZLIB_INCLUDE_DIR "${TOOLCHAIN_DIR}/target/include")
+
+message( ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" )
+message( $ENV{PKG_CONFIG_PATH} )
+message( "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" )
+set(CMAKE_CXX_COMPILER arm-hisiv500-linux-g++)
+set(CMAKE_C_COMPILER   arm-hisiv500-linux-gcc)
+set(GNU_FLAGS "-mfpu=vfp -fPIC")
+set(CMAKE_CXX_FLAGS "${GNU_FLAGS} ")
+set(CMAKE_C_FLAGS "${GNU_FLAGS}  ")
+#从来不在指定目录下查找工具程序
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#只在指定目录下查找库文件
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#只在指定目录下查找头文件
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET(CMAKE_FIND_ROOT_PATH  
+ ${TOOLCHAIN_DIR}/arm-hisiv500-linux-gnueabi/lib )
+
+include_directories(${TOOLCHAIN_DIR}/arm-hisiv500-linux-gnueabi/include/c++/)
+
+  
+ENDIF(CROSS_COMPILE)
---
+++ libwebsockets/build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+if [ "x"${CROSS_COMPILER} == "x" ];then
+    CROSS_COMPILER=arm-hisiv500-linux
+fi
+
+SOURCE_PACKAGE="libwebsockets-v3.1-stable.tar.xz"
+SOURCE_DIR="libwebsockets-v3.1-stable"
+SOURCE_PACKAGE_FULL_PATH=$(pwd)/${SOURCE_PACKAGE}
+SOURCE_DIR_FULL_PATH=$(pwd)/${SOURCE_DIR}
+SOURCE_URL="https://libwebsockets.org/git/libwebsockets/snapshot/libwebsockets-v3.1-stable.tar.xz"
+SOURCE_SHA256SUM="590058463fd8506135ec42704fe22d2555da4037ebc5a0464d99fda92a97efca"
+
+if [ "x"$1 != "x" ];then
+  case $1 in    
+    clean)
+        rm $SOURCE_DIR -r  2> /dev/null
+        ;;
+    install)          
+        pushd ${SOURCE_DIR_FULL_PATH}
+        SYSROOT=$(${CROSS_COMPILER}-gcc --print-sysroot)
+        if [ "x"${SYSROOT} != "x" ];then
+            sudo cp install/* ${SYSROOT}/ -rfd
+        fi
+        if [ "x"${ROOTFS} != "x" ];then
+            cp install/*      ${ROOTFS}/ -rfd
+        fi
+        popd
+        ;;
+    force)
+        ./build.sh clean
+        ./build.sh
+        ;;
+    *)
+        echo './build.sh clean|install'
+  esac
+  exit 0
+
+fi
+
+
+source ../common/common.sh
+
+check_and_get_source_package ${SOURCE_PACKAGE_FULL_PATH} ${SOURCE_URL} ${SOURCE_SHA256SUM}
+#check_and_unpackage_source ${SOURCE_PACKAGE_FULL_PATH} ${SOURCE_DIR_FULL_PATH}
+
+if [ ! -d ${SOURCE_DIR} ];then
+    tar xvf ${SOURCE_PACKAGE}
+    mkdir ${SOURCE_DIR}/build
+    cp ${CROSS_COMPILER}.cmake ${SOURCE_DIR}/build/cross.cmake
+    sed -i '7i\include(${CMAKE_CURRENT_SOURCE_DIR}/build/cross.cmake)' ${SOURCE_DIR}/CMakeLists.txt
+fi
+
+pushd ${SOURCE_DIR}/build
+cmake  ..  && make -j && make install
+#make -j && make install
+if [ $? != 0 ];then
+    echo "编译出错，异常退出..."
+    exit -1
+fi
+
+popd
